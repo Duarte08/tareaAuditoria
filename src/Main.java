e5ed9305934ba548a65b3674d63b0d492a4ef0d3
DG17019ESD1352021 <manuelduart12@gmail.com>
2024-11-18 16:57:50 -0600

    configurando

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,25 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.time.Duration;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+      System.setProperty("webdriver","C:\\Users\\Manuel Duarte\\Downloads\\chrome.exe");
+        WebDriver driver = new ChromeDriver();
+        driver.navigate().to("https://www.google.com");
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
+        WebElement searchbox = driver.findElement(By.name("q"));
+        searchbox.sendKeys("eel ues");
+        searchbox.sendKeys(Keys.RETURN);
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
+        WebElement eel= driver.findElement(By.id("APjFqb"));
+        eel.click();
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
+
+        driver.close();
     }
 }
